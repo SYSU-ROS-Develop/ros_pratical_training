6bc90671785b8e7ea8affaa104d1f787e07fb90c
JerryChan31 <331498453@qq.com>
2018-05-24 14:37:30 +0800

    Map setting up.

--- catkin_ws/build/catkin_generated/setup_cached.sh
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -8,6 +8,8 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/jerrychan/ros_pratical_training/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/jerrychan/ros_pratical_training/catkin_ws/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/jerrychan/ros_pratical_training/catkin_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD="/home/jerrychan/ros_pratical_training/catkin_ws/build"
 export ROSLISP_PACKAGE_DIRECTORIES="/home/jerrychan/ros_pratical_training/catkin_ws/devel/share/common-lisp"
 export ROS_PACKAGE_PATH="/home/jerrychan/ros_pratical_training/catkin_ws/src:$ROS_PACKAGE_PATH"
